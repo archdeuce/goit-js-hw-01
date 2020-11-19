2284ec7cde39d002a16f78826f0a583912e27c2a
Oleg Reshetilo <archdeuce@gmail.com>
2020-11-19 18:42:45 +0200

    fist commit

---
+++ scripts/task-2.js
@@ -0,0 +1,9 @@
+const total = 100;
+const ordered = 100;
+
+const message =
+  ordered > total
+    ? "На складе недостаточно товаров!"
+    : "Заказ оформлен, с вами свяжется менеджер";
+
+console.log(message);
---
+++ scripts/task-5.js
@@ -0,0 +1,43 @@
+const countryName = "КитаЙ";
+
+const CANCELED_BY_USER = "Отменено пользователем!";
+const NO_DELIVERY = "В выбранную страну доставка недоступна.";
+const CHINA = "Китай";
+const AUSTRALIA = "Австралия";
+const INDIA = "Индия";
+const JAMAICA = "Ямайка";
+let message;
+let price = 0;
+let country;
+
+if (countryName === null) {
+  message = CANCELED_BY_USER;
+} else {
+  country =
+    countryName[0].toUpperCase() +
+    countryName.slice(1, countryName.length).toLowerCase();
+
+  switch (country) {
+    case "Китай":
+      price = 100;
+      break;
+    case "Австралия":
+      price = 170;
+      break;
+    case "Индия":
+      price = 80;
+      break;
+    case "Ямайка":
+      price = 120;
+      break;
+    default:
+      message = NO_DELIVERY;
+      break;
+  }
+}
+
+if (price > 0) {
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
+}
+
+console.log(message);
